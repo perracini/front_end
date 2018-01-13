14acde7e75d45fb19a4be604954d64612b3d0fcd
perracini <34664952+perracini@users.noreply.github.com>
2018-01-13 13:36:34 -0200

    Add files via upload

---
+++ estudos_estilizacao/js/moedaConversao.js
@@ -0,0 +1,11 @@
+function numberParaReal(numero) { 
+var formatado = "R$ " + numero.toFixed(2).replace(".", ","); 
+return formatado;
+}
+
+function realParaNumber(texto) { 
+var compativelComParseFloat = texto.replace("R$ ", ""); 
+compativelComParseFloat = compativelComParseFloat.replace(",", "."); 
+var valor = parseFloat(compativelComParseFloat); 
+return valor;
+}
